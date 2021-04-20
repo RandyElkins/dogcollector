381c3a67852212da8f26e4e0d9d187e73e12478d
Randy Elkins <randy.e.elkins@gmail.com>
2021-04-19 20:13:45 -0700

    add the detail functionality for the Dog Collector

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
   path('', views.home, name='home_page'),
   path('about/', views.about, name='about_page'),
   path('dogs/', views.dogs_index, name='dogs_index_page'),
-#   path('dogs/<int:dog_id>/', views.dogs_detail, name='detail'),
+  path('dogs/<int:dog_id>/', views.dogs_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 
 # Our Models
 from .models import Dog
-from .dog_class import dogs
+# from .dog_class import dogs
 # from dog_class.py import Dog
 
 # Create your views here.
@@ -24,10 +24,12 @@ def about(request):
 
 # Define the 'about/' view
 def dogs_index(request):
-    # dogs = Dog.objects.all()
+    dogs = Dog.objects.order_by('id')
     context = { 'dogs': dogs } # context is used for future growth
     print('dogs dogs dogs dogs dogs dogs dogs dogs')
     print(dogs)
     return render(request, 'dogs/index.html', context)
 
-    
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/detail.html', { 'dog': dog })
